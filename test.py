8488832bfcfd80084ad1af00562040332af0a598
lobonexequiel <lobonexequiel@gmail.com>
2023-04-05 11:55:19 -0300

    sin pre-commit

---
+++ test.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from matplotlib.pyplot import Figure
+from matplotlib.pyplot import Axes
+def fun(foo):
+    """docstring"""
+    bar = foo*2
+    return bar
